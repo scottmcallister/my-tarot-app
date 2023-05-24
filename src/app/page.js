689c96dcf6ccbcbe1477e988270479b4b3309d94
Scott McAllister <mr.scott.mcallister@gmail.com>
2023-05-23 23:31:47 -0300

    update styles again

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,16 @@
 import AutoComplete from '@/components/autocomplete';
 import tarotCards from "@/tarot";
-import Link from "next/link";
+import Link from 'next/link';
 
 const Home = () => {
   const tarotNames = tarotCards.map(card => card.name);
   return (
-    <div>
+    <div className="content">
       <h1>DnD Tarot</h1>
       <AutoComplete data={tarotNames} />
-      {/* <ul>
-        {tarotCards.map((card, index) => (
-          <li key={index}>
-            <Link href={`/tarot/${index}`}>
-              {card.name}
-            </Link>
-          </li>
-        ))}
-      </ul> */}
+      <Link href="/tarot/random">
+        <button>Random</button>
+      </Link>
     </div>
   );
 };
